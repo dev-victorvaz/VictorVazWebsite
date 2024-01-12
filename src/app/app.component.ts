5c9a6c1ad997268c7642452f308c26787c73726b
Victor Vaz <triceps_mettle_0f@icloud.com>
2024-01-12 17:14:47 -0300

    Metatags updated on app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
 import { AgeService } from './services/age.service';
 import { CollegeStatusService } from './services/college-status.service';
@@ -8,51 +8,26 @@ import { CollegeStatusService } from './services/college-status.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   constructor(
     private meta: Meta,
     private ageService: AgeService,
-    private collegeStatusService: CollegeStatusService) { }
-
-    ngOnInit(): void {
-      this.meta.addTag({
-        name: 'description', content: `I have ${this.ageService.getAge()} years old, ${this.collegeStatusService.getCollegeStatus()} and
-        I'm very happy to be part of technology area.`
-      });
-      this.meta.addTag({
-        name: 'keywords', content: 'Angular, SQL, DevOPS, Java, Developer, Programmer'
-      });
-      this.meta.addTag({
-        name: 'author', content: 'Victor Vaz'
-      });
-      this.meta.addTag({
-        content: 'https://www.victorvaz.com/assets/images/victor-vaz.jpg', itemprop: 'image'
-      });
-  
-      this.meta.addTag({
-        property: 'og:title', content: 'Victor Vaz'
-      });
-      this.meta.addTag({
-        property: 'og:description', content: `I have ${this.ageService.getAge()} years old, ${this.collegeStatusService.getCollegeStatus()} and
-        I'm very happy to be part of technology area.`
-      });
-      this.meta.addTag({
-        property: 'og:image', content: 'https://www.victorvaz.com/assets/images/victor-vaz.jpg'
-      });
-      this.meta.addTag({
-        property: 'og:image:width', content: '512'
-      });
-      this.meta.addTag({
-        property: 'og:image:height', content: '512'
-      });
-      this.meta.addTag({
-        property: 'og:image:type', content: 'image/jpeg'
-      });
-      this.meta.addTag({
-        name: 'twitter:card', content: 'summary_large_image'
-      });
-      this.meta.addTag({
-        name: 'twitter:image', content: 'https://www.victorvaz.com/assets/images/victor-vaz.jpg'
-      });
+    private collegeStatusService: CollegeStatusService) {
+      this.meta.addTags([
+        {name: 'description', content: `I have ${this.ageService.getAge()} years old, ${this.collegeStatusService.getCollegeStatus()} and
+        I'm very happy to be part of technology area.`},
+        {name: 'author', content: 'Victor Vaz'},
+        {name: 'keywords', content: 'Angular, SQL, DevOPS, Java, Developer, Programmer'},
+        {content: 'https://www.victorvaz.com/assets/images/victor-vaz.jpg', itemprop: 'image'},
+        {property: 'og:title', content: 'Victor Vaz'},
+        {property: 'og:description', content: `I have ${this.ageService.getAge()} years old, ${this.collegeStatusService.getCollegeStatus()} and
+        I'm very happy to be part of technology area.`},
+        {property: 'og:image', content: 'https://www.victorvaz.com/assets/images/victor-vaz.jpg'},
+        {property: 'og:image:width', content: '512'},
+        {property: 'og:image:height', content: '512'},
+        {property: 'og:image:type', content: 'image/jpeg'},
+        {name: 'twitter:card', content: 'summary_large_image'},
+        {name: 'twitter:image', content: 'https://www.victorvaz.com/assets/images/victor-vaz.jpg'}
+      ]);
     }
 }
