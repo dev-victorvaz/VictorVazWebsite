bcafdabe2f3fc4b2fafce187f592dbe724aeb42f
Victor Vaz <triceps_mettle_0f@icloud.com>
2024-01-12 13:41:26 -0300

    Navigation menu optimized.

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,5 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -7,22 +6,5 @@ import { Router, NavigationEnd } from '@angular/router';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent {
-  constructor(private router: Router) { }
-
-  navigateToHome() {
-    this.router.navigate(['/']);
-  }
-
-  navigateToExpertise() {
-    this.router.navigate(['/expertise']);
-  }
-
-  navigateToGitHub() {
-    this.router.navigate(['/github']);
-  }
-
-  navigateToContact() {
-    this.router.navigate(['/contact']);
-  }
   
 }
