9b0c7578271997be62b182d97fa5d4922977df42
Victor Vaz <triceps_mettle_0f@icloud.com>
2024-01-19 13:25:37 -0300

    Dark/Light/System switch added

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -7,7 +7,7 @@ import { Component, Renderer2, ElementRef, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
   private myElement!: HTMLElement;
-  private mobileMenuToggler:boolean = false;
+  private mobileMenuToggler: boolean = false;
 
   constructor(private renderer: Renderer2, private el: ElementRef) { }
 
@@ -15,7 +15,64 @@ export class NavigationComponent implements OnInit {
     this.myElement = this.el.nativeElement.querySelector('#myElementId');
   }
 
-  toggleMobileMenu():void {
+  toggleThemeMode(): void {
+    if (document.documentElement.classList.contains("light")) {
+      document.documentElement.classList.remove("light");
+      document.documentElement.classList.add("dark");
+    } else if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.add("light");
+    } else {
+      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.add("light");
+      }
+    }
+  }
+
+  toggleDarkMode(): void {
+    if (document.documentElement.classList.contains("light")) {
+      document.documentElement.classList.remove("light");
+      document.documentElement.classList.add("dark");
+    } else if (!(document.documentElement.classList.contains("light") || document.documentElement.classList.contains("dark"))) {
+      document.documentElement.classList.add("dark");
+    } else {
+      if (!(window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches)) {
+        document.documentElement.classList.add("light");
+      }
+    }
+  }
+
+  toggleLightMode(): void {
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.add("light");
+    } else if (!(document.documentElement.classList.contains("light") || document.documentElement.classList.contains("dark"))) {
+      document.documentElement.classList.add("light");
+    } else {
+      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.classList.add("light");
+      }
+    }
+  }
+
+  toggleSystemMode(): void {
+    if (document.documentElement.classList.contains("light")) {
+      document.documentElement.classList.remove("light");
+    } else if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+    }
+    /*
+      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.add("light");
+      }
+    */
+  }
+
+  toggleMobileMenu(): void {
     if (this.mobileMenuToggler) {
       this.hideElement();
     }
@@ -39,7 +96,7 @@ export class NavigationComponent implements OnInit {
   showElement() {
     this.renderer.removeClass(this.myElement, 'mobile-hidden');
   }
-  
+
   // Hide the element
   hideElement() {
     this.renderer.addClass(this.myElement, 'mobile-hidden');
